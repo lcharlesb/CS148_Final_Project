eed5b9e9e86846812b1f898255229ce50bd67e9b
Jake Mann <jnmann@uvm.edu>
2019-11-13 14:56:13 -0500

    added content to footer and header and top.php

--- footer.php
+++ footer.php
@@ -1,1 +1,8 @@
-
+<!-- @@@@@@@@@@@@@@@@@@@@@@     Footer            @@@@@@@@@@@@@@@@@@@@@@@@@@ -->
+<footer>
+    <p>Web site designed by Jake Mann, Mateo Riofrio and Luke Beatty.</p>
+    <p>Code for this site can be found at: <a href="https://github.com/lcharlesb/CS148_Final_Project">https://github.com/lcharlesb/CS148_Final_Project</a></p>
+</footer>
+<!-- @@@@@@@@@@@@@@@@@@@@ Ends Footer             @@@@@@@@@@@@@@@@@@@@@@@@@@ -->
+</body>
+</html>
