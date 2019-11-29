6034664012fb20e5d75bd0a5ec2239bf6bd686e3
Luke Beatty <luke.beatty@uvm.edu>
2019-11-29 10:09:20 -0500

    Hash function added to lib.

---
+++ lib/hash_function.php
@@ -0,0 +1,20 @@
+<?php
+print "<!-- BEGIN include hash-function -->";
+
+function hash ($password) {
+    // Hash password
+    $hashedPassword = "";
+    $passwordLength = strlen($password);
+    $passwordAsArray = str_split($password);
+    
+    for ($i=0; $i<$passwordLength; $i++) {
+        $passwordAsArray[$i].replace("a","z").replace("b","y").replace("c","x").replace("d","w").replace("e","v").replace("f","u").replace("g","t").replace("h","s").replace("i","r").replace("j","q").replace("k","p").replace("l","o").replace("m","n").replace("n","m").replace("o","l").replace("p","k").replace("q","j").replace("r","i").replace("s","h").replace("t","g").replace("u","f").replace("v","e").replace("w","d").replace("x","c").replace("y","b").replace("z","a");
+        $hashedPassword .= $passwordAsArray[$i];
+    }
+    
+    return $hashedPassword;
+    
+}
+
+print "<!-- BEGIN include hash-function -->";
+?>
